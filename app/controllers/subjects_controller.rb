ed579675cf2e372c9d4915c27241fed6203859e4
Luis Felipe Miranda <luisfmiranda97@gmail.com>
2018-10-30 16:47:32 -0600

    Basic methods for model controllers

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,74 @@
 class SubjectsController < ApplicationController
+before_action :set_subject, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @subjects = Subject.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @subject = Subject.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @subject = Subject.new(user_params)
+
+    respond_to do |format|
+      if @subject.save
+        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
+        format.json { render :show, status: :created, location: @subject }
+      else
+        format.html { render :new }
+        format.json { render json: @subject.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @subject, notice: 'Subject was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to subjects_url, notice: 'Subject was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @subject = Subject.find(params[:id_subject])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:subject).permit(:name_subject)
+    end
 end
