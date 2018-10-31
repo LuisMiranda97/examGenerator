e2bb74ad33fcd20d0d719fba82621822b615cf22
Luis Bravo <luis.g.bravo1@gmail.com>
2018-10-31 15:08:01 -0600

    Deleted home css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181031020656) do
+ActiveRecord::Schema.define(version: 20181031023000) do
 
   create_table "dynamics", force: :cascade do |t|
     t.string "text"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20181031020656) do
 
   create_table "exams", force: :cascade do |t|
     t.string "name_exam"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "examtopics", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
