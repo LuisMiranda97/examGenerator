c9c44f8957b36a9360825fab42523f5621fb7a5c
Luis Bravo <luis.g.bravo1@gmail.com>
2018-11-12 20:04:41 -0600

    some other things

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   resources :topics
   devise_for :users
 
-  devise_scope :user do
-    root 'devise/sessions#new'
-  end
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
