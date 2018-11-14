0f7f76f37fb1594565f1241ec77fcb0f6690fc25
aliciacisnerosm <aliciacisnerosm@gmail.com>
2018-11-14 00:53:12 -0600

    log out

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   resources :subjects
   resources :topics
   devise_for :users
-
   root 'home#index'
+
+ devise_scope :user do
+    delete 'signout', to: 'devise/sessions#destroy', as: :sign_in
+  end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
